60ffbb40379bca9354912134b2dfe6b59c28a98b
mauripavan <mauriciopavan60@gmail.com>
2023-07-17 19:49:44 -0300

    feat: wip reservations view

--- my-turno-web/src/app/sections/Reservations.tsx
+++ my-turno-web/src/app/sections/Reservations.tsx
@@ -3,22 +3,24 @@
 import Calendar from "react-calendar";
 import CheckboxStep from "../components/CheboxStep";
 import CustomButton, { ButtonVariants, Size } from "../components/CustomButton";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { CALENDAR_TYPES } from "react-calendar/dist/cjs/shared/const";
-import { isSunday } from "date-fns";
+import { isSunday, differenceInDays } from "date-fns";
+import { ICheckboxStepProps } from "../components/CheboxStep/types";
+import TextInput from "../components/TextInput";
 
-const reservationSteps = [
+const reservationSteps: ICheckboxStepProps[] = [
   {
     name: "Elegí tu sucursal",
     number: 1,
-    completed: true,
-    active: false,
+    completed: false,
+    active: true,
   },
   {
     name: "Selecciona el día",
     number: 2,
     completed: false,
-    active: true,
+    active: false,
   },
   {
     name: "Completa el formulario",
@@ -28,82 +30,159 @@ const reservationSteps = [
   },
 ];
 
-const sucursales = ["Villa Crespo", "Palermo", "Colegiales"];
+const sucursales = [
+  "Seleccioná una sucursal",
+  "Villa Crespo",
+  "Palermo",
+  "Colegiales",
+];
+
+const horarios = [
+  "Seleccioná un horario",
+  "De 9:00 a 13:00 hs",
+  "De 15:00 a 19:00 hs",
+];
 
 export default function Reservations() {
-  const [selectedDate, setSelectedDate] = useState(new Date());
   const today = new Date();
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
+  const [steps, setSteps] = useState(reservationSteps);
+  const [branchSelected, setBranchSelected] = useState("");
 
-  // Función para deshabilitar ciertas fechas
+  useEffect(() => {
+    console.log("branchSelected", branchSelected);
+  }, [branchSelected]);
+
   const disableDates = (date: Date, view?: string) => {
     if (view === "month") {
-      if (date < new Date()) return true;
+      const diff = differenceInDays(date, new Date());
+      if (diff < 0) return true;
     }
     if (view === "year") {
       if (date.getMonth() < today.getMonth()) return true;
     }
+    if (isSunday(date) && view === "month") return true;
     return false;
   };
 
-  // Función para agregar clases personalizadas a los días
   const tileClassName = ({ date, view }: any) => {
-    // Aquí puedes personalizar las clases según tus necesidades
-    console.log("DATE===>", date);
-
     let classes = "text-center py-3 font-medium text-base rounded";
-
     if (selectedDate && selectedDate.toDateString() === date.toDateString()) {
       classes += " bg-principal text-white";
     }
-
     if (isSunday(date) && view === "month") {
       classes += " text-gray-4 cursor-not-allowed";
     } else if (disableDates(date, view)) {
-      classes += " text-gray-5 bg-gray-2 cursor-not-allowed";
+      classes += " text-gray-4 cursor-not-allowed";
     } else {
       classes += " cursor-pointer";
     }
-
     return classes;
   };
 
   const handleDateSelect = (date: any) => {
     if (disableDates(date)) return;
     setSelectedDate(date);
+    const newStepValue = [...steps];
+    newStepValue[1] = {
+      completed: true,
+      active: false,
+      name: "Selecciona el día",
+      number: 2,
+    };
+    newStepValue[2] = {
+      completed: false,
+      active: true,
+      name: "Completá el formulario",
+      number: 3,
+    };
+    setSteps(newStepValue);
   };
 
-  useEffect(() => {
-    console.log("selectedDate", selectedDate);
-  }, [selectedDate]);
+  const handleBranchSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    setBranchSelected(e.target.value);
+    const newStepValue = [...steps];
+    newStepValue[0] = {
+      completed: true,
+      active: false,
+      name: "Elegí tu sucursal",
+      number: 1,
+    };
+    newStepValue[1] = {
+      completed: false,
+      active: true,
+      name: "Selecciona el día",
+      number: 2,
+    };
+    setSteps(newStepValue);
+  };
 
   return (
-    <div className="flex flex-col w-full pt-12 pb-10 px-24">
+    <div className="flex flex-col w-full pt-12 pb-10 px-8 lg:px-24">
       <p className="font-semibold text-xl mb-6">Hace una reserva</p>
-      <div className="grid grid-cols-7 gap-x-10">
-        <div className="flex flex-col col-span-4 bg-white px-10 pt-8 pb-11 rounded-lg">
+      <div className="grid grid-cols-7 gap-10">
+        <div className="flex flex-col col-span-7 lg:col-span-4 bg-white px-10 pt-8 pb-11 rounded-lg">
           <p className="font-semibold text-lg mb-1">Reserva</p>
           <p className="font-normal text-sm mb-1">Selecciona tu sercural</p>
 
           <div className="grid grid-cols-3 mt-6">
-            {reservationSteps.map((item, key) => {
+            {steps.map((item, key) => {
               return (
                 <CheckboxStep
                   key={key}
-                  stepName={item.name}
-                  stepNumber={item.number}
+                  name={item.name}
+                  number={item.number}
                   completed={item.completed}
-                  activeStep={item.active}
+                  active={item.active}
                 />
               );
             })}
           </div>
 
           <div className="mt-6">
-            <select className="w-full py-3 px-2 rounded-lg border border-color-gray-3">
+            <select
+              defaultValue={"Seleccioná una sucursal"}
+              className="w-full py-3 px-2 rounded-lg border border-color-gray-3"
+              onChange={(e) => handleBranchSelect(e)}
+            >
               {sucursales.map((item, key) => {
-                return <option key={`sucursale-${key}`}>{item}</option>;
+                return (
+                  <option disabled={key === 0} key={`sucursale-${key}`}>
+                    {item}
+                  </option>
+                );
               })}
             </select>
+          </div>
+          <div className={`${selectedDate ? "visible" : "hidden"}`}>
+            <div className="mt-6">
+              <p className="text-sm mb-0.5">Horario</p>
+              <select
+                defaultValue={"Seleccioná un horario"}
+                className="w-full py-3 px-2 rounded-lg border border-color-gray-3"
+              >
+                {horarios.map((item, key) => {
+                  return (
+                    <option disabled={key === 0} key={`sucursale-${key}`}>
+                      {item}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className="grid grid-cols-2 gap-x-4">
+              <div className="mt-6 col-span-1">
+                <TextInput label="Nombre y Apellido" />
+              </div>
+
+              <div className="mt-6 col-span-1">
+                <TextInput label="Telefono" />
+              </div>
+
+              <div className="mt-6 col-span-2">
+                <TextInput label="Mail" />
+              </div>
+            </div>
           </div>
 
           <div className=" mt-8">
@@ -115,8 +194,13 @@ export default function Reservations() {
             />
           </div>
         </div>
-
-        <div className="col-span-3 bg-white px-10 pt-8 pb-11 rounded-lg">
+        <div
+          className={`h-fit col-span-7 lg:col-span-3 bg-white px-10 pt-8 pb-11 rounded-lg ${
+            !steps[1].active &&
+            !steps[1].completed &&
+            "opacity-50 cursor-not-allowed z-20"
+          } `}
+        >
           <Calendar
             value={selectedDate}
             calendarType={CALENDAR_TYPES.HEBREW}
@@ -125,7 +209,7 @@ export default function Reservations() {
               ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"][date.getDay()]
             }
             minDate={new Date()}
-            className={"text-center font-sans font-normal text-gray-7 text-lg"}
+            className={"text-center font-sans font-noraml text-gray-7 text-lg"}
             tileClassName={tileClassName}
             nextLabel={null}
             next2Label={null}
@@ -133,7 +217,10 @@ export default function Reservations() {
             prev2Label={null}
             minDetail="year"
             locale="es"
-            onClickDay={handleDateSelect}
+            onClickDay={
+              ((steps[1].active || steps[1].completed) && handleDateSelect) ||
+              undefined
+            }
           />
         </div>
       </div>
