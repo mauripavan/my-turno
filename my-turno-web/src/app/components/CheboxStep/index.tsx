e82128ae48857fd75b362a9fc54b23f82707a1d8
mauripavan <mauriciopavan60@gmail.com>
2023-07-17 19:48:58 -0300

    refactor: update on CbeckboxStep Component

--- my-turno-web/src/app/components/CheboxStep/index.tsx
+++ my-turno-web/src/app/components/CheboxStep/index.tsx
@@ -2,42 +2,38 @@ import CheckIcon from "@component/app/assets/icons/Check";
 import { ICheckboxStepProps } from "./types";
 
 const CheckboxStep = (props: ICheckboxStepProps) => {
-  const { stepName, stepNumber, completed, activeStep } = props;
+  const { name, number, completed, active } = props;
   return (
     <div className="flex flex-col items-center">
       <div className="flex w-full items-center mb-2">
         <div
           className={`flex flex-1 h-0.5 ${
-            completed ? "bg-success" : activeStep ? "bg-principal" : "bg-gray-4"
+            completed ? "bg-success" : active ? "bg-principal" : "bg-gray-4"
           }`}
         />
         <div
           className={`flex  items-center justify-center w-8 h-8 rounded-lg ${
-            completed ? "bg-success" : activeStep ? "bg-principal" : "bg-gray-4"
+            completed ? "bg-success" : active ? "bg-principal" : "bg-gray-4"
           } `}
         >
           {completed ? (
             <CheckIcon className="w-3 h-2" />
           ) : (
-            <p className="font-semibold text-sm text-white">{stepNumber}</p>
+            <p className="font-semibold text-sm text-white">{number}</p>
           )}
         </div>
         <div
           className={`flex flex-1 h-0.5 ${
-            completed ? "bg-success" : activeStep ? "bg-principal" : "bg-gray-4"
+            completed ? "bg-success" : active ? "bg-principal" : "bg-gray-4"
           }`}
         />
       </div>
       <p
-        className={`text-sm ${
-          completed
-            ? "text-success"
-            : activeStep
-            ? "text-principal"
-            : "text-gray-4"
+        className={`text-sm text-center ${
+          completed ? "text-success" : active ? "text-principal" : "text-gray-4"
         }`}
       >
-        {stepName}
+        {name}
       </p>
     </div>
   );
